64e8a4233dc1a5985a502ce873e3d732d54b3673
Pravesh <praveshpansari@gmail.com>
2019-10-15 12:49:50 +0545

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,22 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    del = require('del'),
+    browserSync = require('browser-sync').create(),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
 function sassCompile() {
     return gulp.src('./css/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
-};
-
-exports.sassCompile = sassCompile
-
-
-// gulp.task('browser-sync', function() {
-//     var files = [
-//         './*.html',
-//         './css/*.css',
-//         './img/*.{png,jpg,gif}',
-//         './js/*.js'
-//     ];
-
-//     browserSync.init(files, {
-//         server: { baseDir: "./"}
-//     });
-// });
+}
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -35,6 +27,40 @@ gulp.task('browserSync', function() {
     gulp.watch('./css/*.scss', sassCompile)
 });
 
+function clean() {
+    return del(['dist']);
+}
+
+function copyfonts() {
+    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+}
+
+function minifyimg() {
+    return gulp.src('img/*.{png,jpg,gif}')
+    .pipe(imagemin({ optimization:3, progressive: true, interlaced: true}))
+    .pipe(gulp.dest('dist/img'));
+}
+
+function useMin() {
+    return gulp.src('./*.html')
+    .pipe(flatmap(function(stream, file) {
+        return stream
+        .pipe(usemin({
+            css: [rev()],
+            html: [function() { return htmlmin({ collapseWhitespace: true})}],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), 'concat']
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+}
+
+exports.clean = clean;
+exports.copyfonts = copyfonts;
+exports.minifyimg = minifyimg;
 
 gulp.task('default',  gulp.series('browserSync'));
+gulp.task('build', gulp.series(clean, gulp.parallel(copyfonts, minifyimg, useMin)));
 
