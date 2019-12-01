40c902ad6d2be12c992907f9677d55875d0dc9d1
Pravesh Pansari <praveshpansari@gmail.com>
2019-12-01 19:10:22 +0545

    Library System

---
+++ Software Construction/Data Abstraction/Data Abstraction/LibrarySystem/src/model/library/Librarian.java
@@ -0,0 +1,46 @@
+package model.library;
+
+import model.book.Book;
+
+public class Librarian {
+
+    private String name;
+    private int age;
+    private Library managingLibrary;
+    private Book favBook;
+
+    public Librarian(String name, int age, Library lib, Book bk) {
+        this.name = name;
+        this.age = age;
+        this.managingLibrary = lib;
+        this.favBook = bk;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public Library getManagingLibrary() {
+        return managingLibrary;
+    }
+    public Book getFavBook() {
+        return favBook;
+    }
+
+    // REQUIRES: lib != null
+    // MODIFIES: this
+    // EFFECTS: changes this librarian's managing library to the one given.
+    //          Look carefully at the implementation of library - does it
+    //          also have an associated librarian field? Does it make a
+    //          difference in the implementation of this method?
+    public boolean changeLibrary(Library lib) {
+       this.managingLibrary = lib;
+       lib.hireLibrarian(this);
+       return true;
+    }
+
+
+}
