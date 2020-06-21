9dc4112ee9a7389e03f68bcf3f2daf802c2a502d
Pravesh Pansari <praveshpansari@gmail.com>
2020-06-21 11:27:34 +0545

    Component part 1

--- Full-Stack-Coursera/Front-End-JavaScript-Frameworks-Angular/src/app/app.module.ts
+++ Full-Stack-Coursera/Front-End-JavaScript-Frameworks-Angular/src/app/app.module.ts
@@ -5,20 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
     FlexLayoutModule
   ],
   providers: [],
