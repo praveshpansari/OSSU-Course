c0b67c638faa8407ce4189fb50430a7bb9f29d31
Pravesh Pansari <praveshpansari@gmail.com>
2020-06-28 18:49:19 +0545

    Project 4

--- Nand-to-Tetris/projects/04/fill/Fill.asm
+++ Nand-to-Tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,157 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// addr = SCREEN
+// rows = 256
+// columns = 32
+// i = 1
+
+// LOOP1:
+    // if i > rows goto END1
+    // j = 1
+    // LOOP2:
+        // if j > 32 goto END2
+        // RAM[addr] = -1
+        // addr = addr + 1 //go to next column
+        // j = j + 1
+        // goto LOOP2
+    // END2:
+        // i = i + 1;
+        // goto LOOP1
+// END1
+    // goto END1;
+    
+    
+M = 0;
+
+(START)
+@KBD
+D = M;
+
+@CLEAR
+D;JEQ
+
+@SCREEN
+D = A
+@addr
+M = D       // addr = 16384
+
+@256
+D = A
+@rows
+M = D       // rows = 256
+
+@32
+D = A
+@columns
+M = D       // columns = 32
+
+@i
+M = 1       // i = 1
+
+(LOOP1)
+    @i
+    D = M
+    @rows
+    D = D - M
+    @END1
+    D;JGT       // if i > rows goto END1
+
+    @j
+    M = 1       // j = 1
+
+    (LOOP2)
+        @j
+        D = M
+        @columns
+        D = D - M
+        @END2
+        D;JGT       // if j > columns goto END2
+
+        @addr
+        A = M
+        M = -1      // RAM[addr] = -1
+
+
+        @addr
+        M = M + 1       // addr = addr + 1
+
+        @j
+        M = M + 1       // j = j + 1
+
+        @LOOP2
+        0;JMP
+    
+(END2)
+    @i
+    M = M + 1       // i = i + 1
+
+    @LOOP1
+    0;JMP
+
+(END1)
+    @START
+    0;JMP
+
+(CLEAR)
+@SCREEN
+D = A
+@addr
+M = D       // addr = 16384
+
+@256
+D = A
+@rows
+M = D       // rows = 256
+
+@32
+D = A
+@columns
+M = D       // columns = 32
+
+@i
+M = 1       // i = 1
+
+(LOOP3)
+    @i
+    D = M
+    @rows
+    D = D - M
+    @END3
+    D;JGT       // if i > rows goto END1
+
+    @j
+    M = 1       // j = 1
+
+    (LOOP4)
+        @j
+        D = M
+        @columns
+        D = D - M
+        @END4
+        D;JGT       // if j > columns goto END2
+
+        @addr
+        A = M
+        M = 0      // RAM[addr] = 0
+
+
+        @addr
+        M = M + 1       // addr = addr + 1
+
+        @j
+        M = M + 1       // j = j + 1
+
+        @LOOP4
+        0;JMP
+    
+(END4)
+    @i
+    M = M + 1       // i = i + 1
+
+    @LOOP3
+    0;JMP
+
+    (END3)
+    @START
+    0;JMP
--- Nand-to-Tetris/projects/04/mult/mult.asm
+++ Nand-to-Tetris/projects/04/mult/mult.asm
@@ -7,3 +7,62 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// for(i=0,i<R1;i++) {
+    // Add R0 to R2 
+// }
+
+// i = 1;
+// product = 0;
+// multiplier = RAM[1]
+// multiplicand = RAM[0]
+// LOOP:
+    // if i > multiplier goto END
+    // product = product + multiplicand
+    // i = i + 1
+    // goto LOOP
+// END:
+    // RAM[2] = product
+
+@i
+M = 1;      // i = 1
+
+@R2
+M = 0;
+
+@product
+M = 0;      // product = 0
+
+@R1
+D = M
+@multiplier
+M = D       // multiplier = RAM[1]
+
+@R0
+D = M
+@multiplicand
+M = D       // multiplicand = RAM[0]
+
+(LOOP)
+    @i
+    D = M
+    @multiplier
+    D = D - M
+    @END
+    D;JGT      // if i > multiplier goto END
+
+    @multiplicand
+    D = M
+    @product
+    M = D + M       // product = product + 1
+
+    @i
+    M = M + 1       // i = i + 1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @product
+    D = M
+    @R2
+    M = D           // RAM[2] = product
