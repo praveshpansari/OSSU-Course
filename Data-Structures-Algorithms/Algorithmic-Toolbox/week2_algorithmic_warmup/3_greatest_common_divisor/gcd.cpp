eede8267dd92b6c7c8b839a9bcee8002911d2d0a
praveshpansari <praveshpansari@gmail.com>
2020-12-15 22:37:27 +0545

    Week-3 PA-3

--- Data-Structures-Algorithms/Algorithmic-Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ Data-Structures-Algorithms/Algorithmic-Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
-int gcd_naive(int a, int b) {
+int gcd_naive(int a, int b)
+{
   int current_gcd = 1;
-  for (int d = 2; d <= a && d <= b; d++) {
-    if (a % d == 0 && b % d == 0) {
-      if (d > current_gcd) {
+  for (int d = 2; d <= a && d <= b; d++)
+  {
+    if (a % d == 0 && b % d == 0)
+    {
+      if (d > current_gcd)
+      {
         current_gcd = d;
       }
     }
@@ -12,9 +16,18 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
-int main() {
+int gcd_fast(int a, int b)
+{
+  if (b == 0)
+    return a;
+
+  return gcd_fast(b, a % b);
+}
+
+int main()
+{
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_fast(a, b) << std::endl;
   return 0;
 }
