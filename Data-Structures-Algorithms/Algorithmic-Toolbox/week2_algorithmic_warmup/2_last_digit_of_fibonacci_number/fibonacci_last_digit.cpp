e69a867900c91df7bfac6b6ad9ac2cac5c267028
praveshpansari <praveshpansari@gmail.com>
2020-12-15 22:29:42 +0545

    Week-2-PA-2

--- Data-Structures-Algorithms/Algorithmic-Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ Data-Structures-Algorithms/Algorithmic-Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-
-int get_fibonacci_last_digit_naive(int n) {
+#include <vector>
+int get_fibonacci_last_digit_naive(int n)
+{
     if (n <= 1)
         return n;
 
     int previous = 0;
-    int current  = 1;
+    int current = 1;
 
-    for (int i = 0; i < n - 1; ++i) {
+    for (int i = 0; i < n - 1; ++i)
+    {
         int tmp_previous = previous;
         previous = current;
         current = tmp_previous + current;
@@ -16,9 +18,28 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
-int main() {
+int get_fibonacci_last_digit_fast(int n)
+{
+    if (n <= 1)
+        return n;
+
+    int previous = 0;
+    int current = 1;
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        int tmp_previous = previous % 10;
+        previous = current % 10;
+        current = (tmp_previous + current) % 10;
+    }
+
+    return current;
+}
+
+int main()
+{
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    int c = get_fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
-    }
+}
