7907ee51457d885dc91906d388926c419216bb55
praveshpansari <praveshpansari@gmail.com>
2021-01-07 13:29:46 +0545

    Week2-PA6,7

--- Data-Structures-Algorithms/Algorithmic-Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ Data-Structures-Algorithms/Algorithmic-Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-
-int fibonacci_sum_naive(long long n) {
+#include <vector>
+int fibonacci_sum_naive(long long n)
+{
     if (n <= 1)
         return n;
 
     long long previous = 0;
-    long long current  = 1;
-    long long sum      = 1;
+    long long current = 1;
+    long long sum = 1;
 
-    for (long long i = 0; i < n - 1; ++i) {
+    for (long long i = 0; i < n - 1; ++i)
+    {
         long long tmp_previous = previous;
         previous = current;
         current = tmp_previous + current;
@@ -18,8 +20,42 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
-int main() {
+long long get_fibonacci_huge_fast(long long n, long long m)
+{
+    long long prev = 0;
+    long long current = 1;
+
+    std::vector<int> periods;
+    periods.push_back(0);
+    periods.push_back(1);
+    for (int i = 0; i < m * m; ++i)
+    {
+
+        long long temp = prev;
+        prev = current;
+        current = (temp + prev) % m;
+
+        if (prev == 0 && current == 1)
+        {
+            periods.pop_back();
+            break;
+        }
+
+        periods.push_back(current);
+    }
+
+    return periods[n % periods.size()];
+}
+
+int fibonacci_sum_fast(long long n)
+{
+    int x = get_fibonacci_huge_fast(n + 2, 10) - 1;
+    return (x < 0) ? 10 + x : x;
+}
+
+int main()
+{
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << fibonacci_sum_fast(n);
 }
--- Data-Structures-Algorithms/Algorithmic-Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ Data-Structures-Algorithms/Algorithmic-Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -2,14 +2,17 @@
 #include <vector>
 using std::vector;
 
-long long get_fibonacci_partial_sum_naive(long long from, long long to) {
+long long get_fibonacci_partial_sum_naive(long long from, long long to)
+{
     long long sum = 0;
 
     long long current = 0;
-    long long next  = 1;
+    long long next = 1;
 
-    for (long long i = 0; i <= to; ++i) {
-        if (i >= from) {
+    for (long long i = 0; i <= to; ++i)
+    {
+        if (i >= from)
+        {
             sum += current;
         }
 
@@ -21,8 +24,42 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
-int main() {
+long long get_fibonacci_huge_fast(long long n, long long m)
+{
+    long long prev = 0;
+    long long current = 1;
+
+    std::vector<int> periods;
+    periods.push_back(0);
+    periods.push_back(1);
+    for (int i = 0; i < m * m; ++i)
+    {
+
+        long long temp = prev;
+        prev = current;
+        current = (temp + prev) % m;
+
+        if (prev == 0 && current == 1)
+        {
+            periods.pop_back();
+            break;
+        }
+
+        periods.push_back(current);
+    }
+
+    return periods[n % periods.size()];
+}
+
+long long get_fibonacci_partial_sum_fast(long long m, long long n)
+{
+    int x = get_fibonacci_huge_fast(n + 2, 10) - get_fibonacci_huge_fast(m + 1, 10);
+    return (x < 0) ? 10 + x : x;
+}
+
+int main()
+{
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    std::cout << get_fibonacci_partial_sum_fast(from, to) << '\n';
 }
