35fd6ef885381b6deee8b1044fc62a004b535155
Pravesh Pansari <praveshpansari@gmail.com>
2021-02-14 15:33:13 +0545

    lesson(android): Unit1 2.2 coding challenge

--- Android-Google/Unit1/Lesson2/2.2_ShoppingList/app/src/main/java/com/example/shoppinglist/ListActivity.java
+++ Android-Google/Unit1/Lesson2/2.2_ShoppingList/app/src/main/java/com/example/shoppinglist/ListActivity.java
@@ -2,6 +2,7 @@ package com.example.shoppinglist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,24 +13,29 @@ import java.util.ArrayList;
 public class ListActivity extends AppCompatActivity {
 
     public static final String EXTRA_LIST = "com.android.shoppinglist.extra.LIST";
-    public ArrayList<String> SHOPPING_LIST = new ArrayList<>();
+    private ArrayList<String> shoppingList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        Intent intent = getIntent();
+        shoppingList = intent.getStringArrayListExtra(EXTRA_LIST);
+        if (shoppingList != null)
+            for (String s : shoppingList)
+                Log.d("INtent", s);
     }
 
 
     public void listEntry(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         String listItem = ((Button) view).getText().toString();
-        if (SHOPPING_LIST.contains(listItem)) {
-            SHOPPING_LIST.remove(listItem);
+        if (shoppingList.contains(listItem)) {
+            shoppingList.remove(listItem);
         } else {
-            SHOPPING_LIST.add(listItem);
+            shoppingList.add(listItem);
         }
-        intent.putStringArrayListExtra(EXTRA_LIST, SHOPPING_LIST);
+        intent.putStringArrayListExtra(EXTRA_LIST, shoppingList);
         setResult(RESULT_OK, intent);
         finish();
     }
--- Android-Google/Unit1/Lesson2/2.2_ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ Android-Google/Unit1/Lesson2/2.2_ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.shoppinglist;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,16 +14,49 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1;
+    private final ArrayList<TextView> SHOPPING_LIST_VIEWS = new ArrayList<>();
+    public ArrayList<String> shoppingList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item1));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item2));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item3));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item4));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item5));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item6));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item7));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item8));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item9));
+        SHOPPING_LIST_VIEWS.add(findViewById(R.id.list_item10));
+
+        if (savedInstanceState != null) {
+            shoppingList = savedInstanceState.getStringArrayList("list");
+            if (shoppingList != null)
+                for (int i = 0; i < SHOPPING_LIST_VIEWS.size(); i++) {
+                    SHOPPING_LIST_VIEWS.get(i).setText(null);
+                    if (i < shoppingList.size())
+                        SHOPPING_LIST_VIEWS.get(i).setText(shoppingList.get(i));
+                }
+        }
+
     }
 
     public void addItem(View view) {
         Intent intent = new Intent(this, ListActivity.class);
+        intent.putStringArrayListExtra(ListActivity.EXTRA_LIST, shoppingList);
         startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (!shoppingList.isEmpty()) {
+            outState.putStringArrayList("list", shoppingList);
+        }
     }
 
     @Override
@@ -30,10 +64,12 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
             if (data.hasExtra(ListActivity.EXTRA_LIST)) {
-                ArrayList<String> list = data.getStringArrayListExtra(ListActivity.EXTRA_LIST);
-                if (list != null)
-                    for (String s : list) {
-                        Log.d("List item", s);
+                shoppingList = data.getStringArrayListExtra(ListActivity.EXTRA_LIST);
+                if (shoppingList != null)
+                    for (int i = 0; i < SHOPPING_LIST_VIEWS.size(); i++) {
+                        SHOPPING_LIST_VIEWS.get(i).setText(null);
+                        if (i < shoppingList.size())
+                            SHOPPING_LIST_VIEWS.get(i).setText(shoppingList.get(i));
                     }
             }
         }
